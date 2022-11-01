9cf48b87ed604607b58604108f9d9c9a755c0654
jchoi10 <noreply>
2022-10-31 23:44:31 -0400

    intern school get

--- __test__/Intern.test.js
+++ __test__/Intern.test.js
@@ -5,3 +5,9 @@ test ("create intern", () => {
 
     expect(intern.school).toEqual(expect.any(String));
 });
+
+test ("get intern school", () => {
+    const intern = new Intern("Mike", 32, "mike@example.com", "Columbia University");
+
+    expect(intern.getSchool()).toEqual(expect.any(String));
+});
