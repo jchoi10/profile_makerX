94d8563685e8004215d3bfb9560306444d68f309
jchoi10 <noreply>
2022-10-31 22:10:28 -0400

    getName finished

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,9 +1,15 @@
 const Employee = require('../lib/Employee');
 
 test ("create employee", () => {
-    const employee = new Employee("Michelle", 18, 'michelle.work@example.com');
+    const employee = new Employee("Michelle", 18, "michelle.work@example.com");
 
     expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
+});
+
+test ("get employee name", () => {
+    const employee = new Employee("Michelle", 18, "michelle.work@example.com");
+
+    expect(employee.getName()).toEqual(expect.any(String));
 });
