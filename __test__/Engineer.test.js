554656ceea91d730a14dda6430c66bea4d86253b
jchoi10 <noreply>
2022-10-31 22:37:41 -0400

    engineer get github test pass

---
+++ __test__/Engineer.test.js
@@ -0,0 +1,13 @@
+const Engineer = require('../lib/Engineer');
+
+test ("create Engineer", () => {
+    const engineer = new Engineer("Dave", 24, "davem@example.com", "dave10");
+
+    expect(engineer.github).toEqual(expect.any(String));
+});
+
+test ("get engineer github", () => {
+    const engineer = new Engineer("Dave", 24, "davem@example.com", "dave10");
+
+    expect(engineer.getGithub()).toEqual(expect.any(String));
+});
