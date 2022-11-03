932570eabeb09e89397bafc029003252e62aa93e
jchoi10 <noreply>
2022-11-03 01:03:40 -0400

    add intern html

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -32,7 +32,25 @@ const generateEngineer = function (engineer) {
         </div>
     </div>
     `
-}
+};
+
+const generateIntern = function (intern) {
+    return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${intern.name}</h3>
+                <h4>Intern</h4><i class="bi bi-plus-square-dotted"></i>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${intern.id}</p>
+                <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
+                <p class="school">School: ${intern.school}</p>
+            </div>
+        </div>
+    </div>
+    `
+};
 
 const generateHomepage = function (employeeCards) {
     return `
