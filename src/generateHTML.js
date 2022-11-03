ca4a1d8bb12d164ac3e55763182b007e9346de6e
jchoi10 <noreply>
2022-11-03 00:51:21 -0400

    added manager and engineer cards html

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,3 +1,39 @@
+const generateManager = function (manager) {
+    return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${manager.name}</h3>
+                <h3>Manager</h3><i class="bi bi-clipboard-check">/i>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${manager.id}</p>
+                <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+                <p class="office">Office Number: ${manager.officeNumber}</p>
+            </div>
+        </div>
+    </div>
+    `
+};
+
+const generateEngineer = function (engineer) {
+    return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${engineer.name}</h3>
+                <h3>Engineer</h3><i class="bi bi-hammer"></i>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${engineer.id}</p>
+                <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+            </div>
+        </div>
+    </div>
+    `
+}
+
 const generateHomepage = function (employeeCards) {
     return `
     <!DOCTYPE html>
